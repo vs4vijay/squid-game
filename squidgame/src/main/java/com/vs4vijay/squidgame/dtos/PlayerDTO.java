341d0a91d298a4c5e913b234658250b2ae7c861f
vs4vijay <vs4vijay@gmail.com>
2021-10-19 21:36:57 +0530

    Housekeeping

--- squidgame/src/main/java/com/vs4vijay/squidgame/dtos/PlayerDTO.java
+++ squidgame/src/main/java/com/vs4vijay/squidgame/dtos/PlayerDTO.java
@@ -2,10 +2,9 @@ package com.vs4vijay.squidgame.dtos;
 
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 public class PlayerDTO extends BaseDTO {
     String id;
     String name;
+    String gameId;
 }
